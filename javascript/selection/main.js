bd79600e70e4eb2803929ff9f5c697968e7bfedf
Kai <kailunden9@gmail.com>
2025-02-12 02:16:25 +0000

    Add display of gimmicks in the selection list

--- javascript/selection/main.js
+++ javascript/selection/main.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded",() => {
     currentGimmicks = global.gimmicks.items;
-
+    const gimmickDisplay = document.getElementById("file-list");
+    const nextGimmick = document.createElement("li");
+    for (const gimmick of currentGimmicks) {
+        nextGimmick.innerHTML = currentGimmicks[index];
+    }
 });
